620428c1b7bddf06273549c80205170e0f2044a3
zoe hopkins <zoe.hopkins@gmail.com>
2017-08-12 15:16:10 +0000

    Answer 1.6

---
+++ session1/challenge/6_string_zoe.rb
@@ -0,0 +1,18 @@
+# You'll get a string and a boolean.
+# When the boolean is true, return a new string containing all the odd characters.
+# When the boolean is false, return a new string containing all the even characters.
+# 
+# If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
+
+def odds_and_evens(string, return_odds)
+  new_string = ""
+  string.length.times do |i|
+    next if return_odds && i.even?
+    next if !return_odds && i.odd?
+    new_string << string[i]
+  end
+  new_string
+end
+
+odds_and_evens("abcdefg",true)    # => "bdf"
+odds_and_evens("abcdefg",false)   # => "aceg"
