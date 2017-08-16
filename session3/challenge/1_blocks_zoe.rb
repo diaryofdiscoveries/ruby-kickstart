a6b0986ce6aee0326b38fe7c69bbb4ddb9c61668
zoe hopkins <zoe.hopkins@gmail.com>
2017-08-16 16:18:39 +0000

    Answer 3.1

---
+++ session3/challenge/1_blocks_zoe.rb
@@ -0,0 +1,8 @@
+# Write a method, reverse_map, which invokes a block on each of the elements in reverse order,
+# and returns an array of their results.
+
+def reverse_map(*elements, &block)
+  elements.reverse.map(&block)
+end
+
+# reverse_map(1, 2, 3) { |i| i * 2 }      # => [6, 4, 2]
